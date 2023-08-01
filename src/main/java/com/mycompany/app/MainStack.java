9c5e275e9ec6f100aad9768853824fb897f4bf96
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-08-01 15:22:53 +0200

    chore: gradleified project

---
+++ src/main/java/com/mycompany/app/MainStack.java
@@ -0,0 +1,48 @@
+package com.mycompany.app;
+
+import software.constructs.Construct;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.hashicorp.cdktf.TerraformStack;
+import com.hashicorp.cdktf.TerraformOutput;
+
+import imports.aws.provider.AwsProvider;
+import imports.aws.data_aws_region.DataAwsRegion;
+import imports.aws.dynamodb_table.DynamodbTable;
+import imports.aws.dynamodb_table.DynamodbTableAttribute;
+import imports.aws.sns_topic.SnsTopic;
+
+public class MainStack extends TerraformStack
+{
+    public MainStack(final Construct scope, final String id) {
+        super(scope, id);
+
+        AwsProvider.Builder.create(this, "aws").region("eu-central-1").build();
+
+        DataAwsRegion region = new DataAwsRegion(this, "region");
+
+        DynamodbTable table = DynamodbTable.Builder.create(this, "Hello").name("my-first-table-" + region.getName())
+                .hashKey("temp").attribute(Arrays.asList(DynamodbTableAttribute.builder().name("id").type("S").build()))
+                .billingMode("PAY_PER_REQUEST").build();
+
+        table.addOverride("hash_key", "id");
+        table.addOverride("lifecycle", Collections.singletonMap("create_before_destroy", true));
+
+        final int topicCount = 1;
+        List<SnsTopic> topics = IntStream.range(0, topicCount)
+                .<SnsTopic>mapToObj(
+                        i -> SnsTopic.Builder.create(this, "Topic" + i).displayName("my-first-sns-topic" + i).build())
+                .collect(Collectors.toList());
+
+        TerraformOutput.Builder.create(this, "table_name").value(table.getName()).build();
+
+        for (int i = 0; i < topics.size(); i++) {
+            TerraformOutput.Builder.create(this, "sns_topic" + i).value(topics.get(i).getName()).build();
+        }
+    }
+}
